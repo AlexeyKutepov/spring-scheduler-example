5c7d937dd2eecf53efeb6de2b7903120f5740f14
Alexey.Kutepov <Alexey.Kutepov@russianpost.ru>
2023-10-24 12:21:51 +0300

    Initial commit

---
+++ src/main/java/ru/akutepov/springschedulerexample/service/ScheduledTaskService.java
@@ -0,0 +1,72 @@
+package ru.akutepov.springschedulerexample.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableAsync
+public class ScheduledTaskService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTaskService.class);
+
+    @Scheduled(fixedDelay = 1000)
+    public void scheduleFixedDelayTask() throws InterruptedException {
+        LOGGER.info("scheduleFixedDelayTask: begin");
+        Thread.sleep(2000);
+        LOGGER.info("scheduleFixedDelayTask: end");
+    }
+
+    @Scheduled(fixedRate = 3000)
+    public void scheduleFixedRateTask() throws InterruptedException {
+        LOGGER.info("scheduleFixedRateTask: begin");
+        Thread.sleep(1000);
+        LOGGER.info("scheduleFixedRateTask: end");
+    }
+
+    @Async
+    @Scheduled(fixedRate = 1000)
+    public void scheduleFixedRateTaskAsync() throws InterruptedException {
+        LOGGER.info("scheduleFixedRateTaskAsync: begin");
+        Thread.sleep(3000);
+        LOGGER.info("scheduleFixedRateTaskAsync: end");
+    }
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 10000)
+    public void scheduleFixedRateWithInitialDelayTask() throws InterruptedException {
+        LOGGER.info("scheduleFixedRateWithInitialDelayTask: begin");
+        Thread.sleep(3000);
+        LOGGER.info("scheduleFixedRateWithInitialDelayTask: end");
+    }
+
+    @Scheduled(cron = "0 * * * * ?", zone = "Europe/Moscow")
+    public void scheduleCronExpressionTask() throws InterruptedException {
+        LOGGER.info("scheduleCronExpressionTask: begin");
+        Thread.sleep(3000);
+        LOGGER.info("scheduleCronExpressionTask: end");
+    }
+
+    @Scheduled(fixedDelayString = "${task.fixed.delay.millis}")
+    public void scheduleFixedDelayParametrizedTask() throws InterruptedException {
+        LOGGER.info("scheduleFixedDelayParametrizedTask: begin");
+        Thread.sleep(2000);
+        LOGGER.info("scheduleFixedDelayParametrizedTask: end");
+    }
+
+    @Scheduled(fixedRateString = "${task.fixed.rate.millis}")
+    public void scheduleFixedRateParametrizedTask() throws InterruptedException {
+        LOGGER.info("scheduleFixedRateParametrizedTask: begin");
+        Thread.sleep(2000);
+        LOGGER.info("scheduleFixedRateParametrizedTask: end");
+    }
+
+    @Scheduled(cron = "${task.cron.expression}")
+    public void scheduleCronExpressionParametrizedTask() throws InterruptedException {
+        LOGGER.info("scheduleCronExpressionParametrizedTask: begin");
+        Thread.sleep(2000);
+        LOGGER.info("scheduleCronExpressionParametrizedTask: end");
+    }
+}
---
+++ src/test/java/ru/akutepov/springschedulerexample/SpringSchedulerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akutepov.springschedulerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
